32cd650e5b076007d9d67a2e1347452af9d6fb55
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-06-16 19:30:02 -0400

    apply changes from audit

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,19 +1,22 @@
-pragma solidity ^0.6.6;
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 
 contract Vesting{
 
     using SafeMath for uint256;
-    address public tokenAddress;
+    using SafeERC20 for IERC20;
+    address public immutable tokenAddress;
     address payable claimant;
-    uint256 public period;
+    uint256 public period;//total length of the vesting period
     uint256 public total;//total tokens to send over vesting period
     uint256 public startTime;//start time of the vesting
     uint256 public claimed;//amount claimed so far
+    bool started = false;
 
     constructor(address payable claimant_, address tokenAddress_) public {
         claimant = claimant_;
@@ -35,8 +38,10 @@ contract Vesting{
     }
 
     function start(uint256 amount, uint256 period_) public {
-        ERC20 token = ERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), amount);
+        require(started == false, "already started");
+        IERC20 token = IERC20(tokenAddress);
+        token.safeTransferFrom(msg.sender, address(this), amount);
+        started = true;
         period = period_;
         total = amount;
         startTime = block.timestamp;
@@ -44,18 +49,28 @@ contract Vesting{
 
     function collect() public onlyClaimant returns(uint256) {
         uint256 elapsed = block.timestamp.sub(startTime);
-        ERC20 token = ERC20(tokenAddress);
-        if (elapsed > block.timestamp.add(period)) {
+        IERC20 token = IERC20(tokenAddress);
+        if (elapsed > period) {
             //vesting totally complete
             uint256 amount = total.sub(claimed);
             claimed = total;
-            token.transfer(claimant, amount);
+            token.safeTransfer(claimant, amount);
+            return amount;
         } else {
-            uint256 perComplete = period.div(elapsed);
-            uint256 amount = total.div(perComplete);
-            amount = amount.sub(claimed);
+            uint256 percentDone;
+            uint256 onePercent = period.div(100);
+            while (elapsed > 0) {
+                if (elapsed >= onePercent) {
+                    elapsed = elapsed.sub(onePercent);
+                    percentDone = percentDone.add(1);
+                } else {
+                    elapsed = 0;
+                }
+            }
+            uint256 amount = (total.div(100).mul(percentDone)).sub(claimed);    
+            
             claimed = claimed.add(amount);
-            token.transfer(claimant, amount);
+            token.safeTransfer(claimant, amount);
             return amount;
         }
     }
